fafa7c52e57b28609e00e485118ce25571f13b70
Joy <leedoy7989@gmnail.com>
2021-01-25 19:04:27 +0900

    Currency change App

--- Currency(Exercise_20210123)/ViewController.swift
+++ Currency(Exercise_20210123)/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertResult: UILabel!
+    
+    @IBOutlet weak var convertRate: UITextField!
+    
+    @IBOutlet weak var convertAmount: UITextField!
+    
+    @IBOutlet weak var convertState: UISegmentedControl!
+    
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        convertResult.text = ""
     }
-
+    
+    @IBAction func convertClick(_ sender: Any) {
+        
+        let rate = Float(convertRate.text!)!
+        let amount = Float(convertAmount.text!)!
+        
+        let result = rate * amount
+        
+        if convertState.selectedSegmentIndex == 0 {
+            convertResult.text = " ￦ \(result) "
+        }else {
+            convertResult.text = "﹩\(result) "
+        }
+        
+        
+    }
+    
+    
 
 }
 
