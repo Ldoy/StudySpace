f3a622acea51439107c05bb135922d6571dc4d1f
Joy <leedoy7989@gmnail.com>
2021-01-27 15:52:28 +0900

    toDoList App(Edit, Multiple Views)

---
+++ toDoList/EmojiDictionaryTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiDictionaryTableViewController.swift
+//  EmojiDictionary(0126)
+//
+//  Created by Do Yi Lee on 2021/01/26.
+//
+
+import UIKit
+
+class EmojiDictionaryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+      
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+   
+    }
+
+}
