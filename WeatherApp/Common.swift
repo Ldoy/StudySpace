ede0676c4ecb7fa33821c3c4036a1be4f22635b3
Joy <leedoy7989@gmnail.com>
2021-04-06 19:39:08 +0900

    날씨 API
    날씨 API를 openweathermap  에서 생성 후 swift 파일에 추가

---
+++ WeatherApp/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  WeatherApp
+//
+//  Created by Do Yi Lee on 2021/04/06.
+//
+
+import Foundation
+
+let apiKey = "9cda367698143794391817f65f81c76e"
+
+//https://home.openweathermap.org 에서 받은 API Key
+
