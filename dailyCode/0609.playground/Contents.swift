229007f6db75c4d7cc79ed5bafba684dad9c546c
Ldoy <leedoy7989@gmail.com>
2021-06-09 13:21:37 +0900

    property Observer 연습

--- dailyCode/0609.playground/Contents.swift
+++ dailyCode/0609.playground/Contents.swift
@@ -72,3 +72,82 @@ class AutomaticCar: Car {
 let automatic = AutomaticCar()
 automatic.currentSpeed = 30.0
 print("AutomaticCar: \(automatic.description)")
+
+//computed Property
+ 
+struct Point {
+	var x = 0.0,  y = 0.0
+}
+
+struct Size {
+	var width = 0.0, height = 0.0
+}
+
+struct Rect {
+	var origin = Point()
+	var size = Size()
+	var center: Point {
+		get {
+			let centerX = origin.x + (size.width / 2)
+			let centerY = origin.y + (size.height / 2)
+			return Point(x: centerX, y: centerY)
+		}
+		set {
+			origin.x = newValue.x - (size.width / 2)
+			origin.y = newValue.y - (size.height / 2)
+		}
+	}
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+struct AlternativeRect {
+	var origin = Point()
+	var size = Size()
+	var center: Point {
+		get {
+			let centerX = origin.x + (size.width / 2)
+			let centerY = origin.y + (size.height / 2)
+			return Point(x: centerX, y: centerY)
+		}
+		set {
+			origin.x = newValue.x - (size.width / 2)
+			origin.y = newValue.y - (size.height / 2)
+		}
+	}
+}
+
+struct CompactRect {
+	var origin = Point()
+	var size = Size()
+	var center: Point {
+		get {
+			Point(x: origin.x + (size.width / 2), y: origin.y + (size.height / 2))
+		}
+		set {
+			origin.x = newValue.x - (size.width / 2)
+			origin.y = newValue.y - (size.height / 2)
+		}
+	}
+}
+
+class StepCounter {
+	var totalSteps: Int = 0 {
+		willSet {
+			print(totalSteps)
+		}
+		didSet {
+			if totalSteps > oldValue {
+				print(totalSteps)
+			}
+		}
+	}
+}
+
+
+let test = StepCounter()
+test.totalSteps = 4
+
