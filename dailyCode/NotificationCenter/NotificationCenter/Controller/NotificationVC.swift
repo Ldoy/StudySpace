0fef4588a4c544178a2e31d3d2020ad249509ba9
Ldoy <leedoy7989@gmail.com>
2021-07-23 00:07:01 +0900

    refactor: 주석제거

--- dailyCode/NotificationCenter/NotificationCenter/Controller/NotificationVC.swift
+++ dailyCode/NotificationCenter/NotificationCenter/Controller/NotificationVC.swift
@@ -17,11 +17,8 @@ class NotificationVC: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBAction func postToNotiCenter(_ sender: Any) {
         guard let text = textField.text else { return }
-        // 디폴트 속성으로 기본 제공 기능이용가능 -> 전달할 때는 포스트 사용 , 유저인포를 받는 메소드를 선택하기,
-//        NotificationCenter.default.post(name: <#T##NSNotification.Name#>, object: <#T##Any?#>, userInfo: <#T##[AnyHashable : Any]?#>)
-        //  노티피케이션 전달하는 객체 구분할 때는 옵젝트 파라미터 이용
-        // 유저인포 : 개수의 상관없이 항상 구현해야함. 그래서 두번째 파라미터를 대신 활용하기도 함?
-        NotificationCenter.default.post(name: NSNotification.Name.NewValueDidInput, object: nil, userInfo: ["NewValue" : text])
+     
+ 
     }
     
     override func viewDidLoad() {
