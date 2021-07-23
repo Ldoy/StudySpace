13299790ef41c25fb53b1461b08274b0ebc4675d
Ldoy <leedoy7989@gmail.com>
2021-07-23 23:54:28 +0900

    테이블뷰 처음부터 구현해보기-진행중, multiSection까지 구현

--- dailyCode/DataTransfer_Delegation/DataTransfer_Delegation/Controller/ViewController.swift
+++ dailyCode/DataTransfer_Delegation/DataTransfer_Delegation/Controller/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let list = TodoListSections.generateData()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       //인터페이스로 델리게이트, 데이터 소스 설정
+  
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list[section].items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let target = list[indexPath.section].items[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: target.type.rawValue, for: indexPath)
+        
+        cell.textLabel?.text = target.title
+
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return list[section].headers
+    }
+}
--- dailyCode/DataTransfer_Delegation/DataTransfer_Delegation/Model/TodoList.swift
+++ dailyCode/DataTransfer_Delegation/DataTransfer_Delegation/Model/TodoList.swift
@@ -7,19 +7,40 @@
 
 import Foundation
 
-class TodoList {
-    // section : 3개
+enum CellType: String {
+    case coding
+    case english
+    case travelPlace
+}
+
+class TodoListItems {
+    // section : 3개 - 코딩, 영어, 여행
     // 섹션마다 헤더, 셀에들어갈 아이템(label의 이름)을 가진다
     // 셀의 아이템은 배열타입으로 구현
     // cell의 아이덴티 파이어 enum으로
     
-    
-    
-    
+    init(type: CellType, title: String) {
+        self.type = type
+        self.title = title
+    }
+    let title: String
+    let type: CellType
 }
 
-class TodoListItems {
+class TodoListSections {
+    init( items: [TodoListItems], headers: String, footers: String) {
+        self.items = items
+        self.footers = footers
+        self.headers = headers
+    }
+    let items: [TodoListItems]
+    let footers: String
+    let headers: String
     
+    static func generateData() -> [TodoListSections] {
+        let list = [TodoListSections(items: [TodoListItems(type: .coding, title: "tableView"), TodoListItems(type: .coding, title: "Delegate Pattern"), TodoListItems(type: .coding, title: "life cycle Method")], headers: "👩🏻‍💻", footers: "--------------"), TodoListSections(items: [TodoListItems(type: .english, title: "말하기연습")], headers: "🗣", footers: "--------------"), TodoListSections(items: [TodoListItems(type: .travelPlace, title: "독일")], headers: "🏕", footers: "--------------")]
+        return list
+    }
 }
 
 
