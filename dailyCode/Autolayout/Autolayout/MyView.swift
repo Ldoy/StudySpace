d16762fa01b18cb7cce048a9a2295c6537bc13af
Ldoy <leedoy7989@gmail.com>
2021-08-02 11:57:41 +0900

    오토레이아웃강의 - Intrinsic Content Size 실습

---
+++ dailyCode/Autolayout/Autolayout/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  MyView.swift
+//  Autolayout
+//
+//  Created by Do Yi Lee on 2021/08/02.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+
+}
