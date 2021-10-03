c3ecf23439a36ad0e20a1e96362631ef3d0b7627
Ldoy <leedoy7989@gmail.com>
2021-10-03 22:57:03 +0900

    feat: priority 관련 제약 공부하기

---
+++ dailyCode/Async_AwaitExercise/Async_AwaitExercise/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  Async_AwaitExercise
+//
+//  Created by Do Yi Lee on 2021/10/03.
+//
+
+import Foundation
--- dailyCode/Autolayout/Autolayout/FirstVC.swift
+++ dailyCode/Autolayout/Autolayout/FirstVC.swift
@@ -11,7 +11,26 @@ class FirstVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let button = UIButton()
+        button.setTitle("코드로버튼만들기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemBlue
+        self.view.addSubview(button)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        button.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        // 바텀
+        let buttonBottomToSafeArea = button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        buttonBottomToSafeArea.priority = .defaultLow
+        
+        let buttonBottomToSuperView = button.bottomAnchor.constraint(lessThanOrEqualTo: self.view.bottomAnchor, constant: -20)
+        buttonBottomToSuperView.priority = .defaultHigh
+        print(buttonBottomToSafeArea.priority, buttonBottomToSuperView.priority)
+        buttonBottomToSuperView.isActive = true
+        buttonBottomToSafeArea.isActive = true
+        
+        
     }
     
 
