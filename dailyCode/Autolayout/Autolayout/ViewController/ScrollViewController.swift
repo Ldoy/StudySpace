affebf457025130cad50848efcdf6d79d24276e1
Ldoy <leedoy7989@gmail.com>
2021-10-07 12:12:06 +0900

    feat: 스크롤뷰를 인터페이스빌더에서 구현하기

---
+++ dailyCode/Autolayout/Autolayout/ViewController/ScrollViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ScrollViewController.swift
+//  Autolayout
+//
+//  Created by Do Yi Lee on 2021/10/07.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
